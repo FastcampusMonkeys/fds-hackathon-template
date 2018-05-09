a4a17c0ef48e1644cd22e7a5d6eabc7f1a3a2ec7
kimdongwon <ehddnjs8989@gmail.com>
2018-05-09 20:19:16 +0900

    baseball logic complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,53 @@
-$("#inputBox").keypress(function(event) {
-  if (
-    event.which &&
-    (event.which <= 1 || event.which >= 9) &&
-    event.which != 8
-  ) {
-    event.preventDefault();
+class baseBallGame {
+  randomNumCreate() {
+    randomNumber1 = Math.round(Math.random() * 9);
+    do (randomNumber2 = Math.round(Math.random() * 9))
+    while(randomNumber1 === randomNumber2);
+    do(randomNumber3 = Math.round(Math.random() * 9))
+    while(randomNumber2===randomNumber3 && randomNumber3 === randomNumber1);
   }
-});
+
+  randomNumber = [randomNumber1, randomNumber2, randomNumber3];
+
+  userNumber1 = document.querySelector(".userNumber1");
+  userNumber2 = document.querySelector(".userNumber2");
+  userNumber3 = document.querySelector(".userNumber3");
+
+  userNumber = [userNumber1, userNumber2,userNumber3];
+
+ Checker() { 
+  let s=0,b=0;
+   for(i=0; i < 3; i++){
+    for(j=0; j < 3; j++){
+      if(this.randomNumber[i]===this.userNumber[j] && i === j){
+        s++;
+      }else if(this.randomNumber[i]===this.userNumber[j] && i !==j){
+        b++
+      }else{
+      }
+    }
+}
+return "strike" + `${s}`+ "ball" + `${b}`;
+ }
+}
+
+
+
+
+// 세자리수가 랜덤으로 0~9 까지나오고
+
+// 같은 번호가 아닌
+
+// 정답 3자리수를 컴퓨터가 저장
+
+// —————————————————————
+
+// 시도하는 숫자랑 비교해서 맞는 것을 판단
+
+// 숫자가 위치는 맞지않고 존재하면 ball 개념
+
+// 숫자가 위치까지 맞으면 strikt
+
+// 아무것도 안맞을시  out
+
+// 다맞으면 게임끝
