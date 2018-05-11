4827bfec2de0252358ab27e231fd3e346e2f6d49
OnloadMyBrain <stancode203@gmail.com>
2018-05-11 14:02:09 +0900

    인풋 포커스 적용

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+const answer = document.querySelector('.answer');
+const btnTry = document.querySelector(".input-box__try");
+const btnReset = document.querySelector('.input-box__reset');
+const num1 = document.getElementsByName('userNumber')[0];
+const num2 = document.getElementsByName('userNumber')[1];
+const num3 = document.getElementsByName('userNumber')[2];
+const trial = document.getElementsByName('btnTry')[0];
+const turnList = document.querySelector('.turn-list');
+let count = 0;
+
 class baseBallGame {
   answerArr = [];
   randomNumCreate() {
@@ -15,17 +25,15 @@ class baseBallGame {
   // randomNumber = [randomNumber1, randomNumber2, randomNumber3]; // 배열로 사용하여 비교하기 위해 배열로 정의
 
   // 정답숫자 변수에 저장.
-  randomNumber = this.randomNumCreate()
-
+  randomNumber = this.randomNumCreate();
   userNumber = document.getElementsByName('userNumber');
-
 
   // userNumber = [userNumber1, userNumber2, userNumber3]; // random number와 비교하기 위해 배열로 지정
 
   checker() {
     // 비교시작
-    let s = 0,
-      b = 0; // 이중포문을 통해 인수값과 결과같이 같으면 strike, 결과값만 같으면 ball 둘다 같지 않으면 아웃으로 처리.
+    let s = 0, b = 0, c='OUT'; // 이중포문을 통해 인수값과 결과같이 같으면 strike, 결과값만 같으면 ball 둘다 같지 않으면 아웃으로 처리.
+    let answerMsg = '';
     for (i = 0; i < 3; i++) {
       for (j = 0; j < 3; j++) {
         if (this.randomNumber[i].toString() === this.userNumber[j].value && i === j) {
@@ -33,9 +41,16 @@ class baseBallGame {
         } else if (this.randomNumber[i].toString() === this.userNumber[j].value && i !== j) {
           b++;
         }
+        // else if(this.randomNumber[i].toString() !== this.userNumber[j].value && i !== j) {
+        //   answerMsg = c;
+        // }
+        if(s === 3){
+          answer.textContent = '정답입니다! 정답은' + this.randomNumber + ' 입니다.';
+        }
       }
     }
-    return s + "strike " + b + "ball";
+    
+    return answerMsg = s + "strike " + b + "ball";
   }
 
 }
@@ -43,22 +58,12 @@ class baseBallGame {
 const game = new baseBallGame();
 
 // 변경사항
-const btnTry = document.querySelector(".input-box__try"); // DOM 설정  시작!! 박스로 userNumber값 넘기기
 
-const btnReset = document.querySelector('.input-box__reset');
-
-let num1 = document.getElementsByName('userNumber')[0];
-let num2 = document.getElementsByName('userNumber')[1];
-let num3 = document.getElementsByName('userNumber')[2];
-let trial = document.getElementsByName('trial');
-let answer = document.querySelector('.answer');
-let count = 0;
-
-btnTry.addEventListener('click', function () {
+btnTry.addEventListener('click', function (e) {
   let iDiv = document.createElement('div');
   iDiv.className = 'score-box';
   count++;
-  document.querySelector('.turn-list').appendChild(iDiv);
+  turnList.appendChild(iDiv);
   iDiv.textContent = count + '회 ====== ' + num1.value + ' ' + num2.value + ' ' + num3.value + ' ==== ' + game.checker();
 
   // 이부분 코딩중입니다!
@@ -69,11 +74,12 @@ btnTry.addEventListener('click', function () {
   num1.value = "";
   num2.value = "";
   num3.value = "";
-  
   num1.focus();
 
   if(count === 9){
-    answer.textContent = '정답은 ' + game.randomNumber + ' 입니다.'
+    answer.textContent = '정답은 ' + game.randomNumber + ' 입니다.';
+    btnTry.setAttribute('disabled','disabled');
+    btnReset.focus();
   }
 });
 
@@ -91,8 +97,9 @@ num2.addEventListener('keypress', function () {
   }
 });
 // 이부분 입니다!
-num3.addEventListener('keypress', function () {
-  if (num3.value !== '' || num3.value !== null) {
+num3.addEventListener('keyup', function () {
+  if (num3.value !== '' || num3.value === null) {
+    
     trial.focus();
   }
 });
@@ -100,7 +107,6 @@ num3.addEventListener('keypress', function () {
 btnReset.addEventListener('click', () => {
   location.replace('');
 });
-
 
 
 // console.log(game.randomNumCreate())
